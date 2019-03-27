f56d1a6e85c8fd0fe536c877091edee3a488b2f7
Pooja Charla <poojareddycharla@gmail.com>
2019-03-26 22:19:34 -0500

    reset to no errors

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -8,7 +8,6 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {  
-    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
